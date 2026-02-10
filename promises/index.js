b7470bdfa56272388dfef03fabea8ca140d6d07f
shubham-Alhat <shubhamalhat520@gmail.com>
2026-02-10 07:50:23 +0530

    promises completed

--- promises/index.js
+++ promises/index.js
@@ -1,3 +1,5 @@
+import { d } from "./date.index.js";
+
 class Rectangle {
   constructor(width, height, color) {
     this.width = width;
@@ -5,11 +7,37 @@ class Rectangle {
     this.color = color;
   }
 
+  // this is static methods
+  static whoAmI() {
+    return "I am Rectangle";
+  }
+
+  // this is non static method
   getarea() {
     const area = this.width * this.height;
     return area;
   }
+
+  // this is non static method
+  parameter() {
+    return 2 * (this.width * this.height);
+  }
 }
 
-let r1 = new Rectangle(20, 30, "black");
+const r1 = new Rectangle(20, 20, "blue");
+const r2 = new Rectangle(10, 15, "red");
+
 console.log(r1.getarea());
+console.log(r1.parameter());
+
+console.log("\n");
+
+console.log(r2.getarea());
+console.log(r2.parameter());
+console.log(r2.color);
+
+// static methods dont get attach to internals objects but they attached to direct class itself. in this case  - Rectangle
+
+// console.log(r1.whoAmI())       // this will throw error
+
+console.log(Rectangle.whoAmI());
